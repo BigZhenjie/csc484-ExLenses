2e56fd42d5d05a7d5ea4ba05c29b8233e5425332
BigZhenjie <1741zhenjie@gmail.com>
2024-06-11 20:56:59 -0700

    Add router functionality to NavBar component

---
+++ my-app/src/app/(root)/link-account/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LinkAccount = () => {
+  return (
+    <div>LinkAccount</div>
+  )
+}
+
+export default LinkAccount
--- my-app/src/components/NavBar.tsx
+++ my-app/src/components/NavBar.tsx
@@ -6,10 +6,11 @@ import { motion } from "framer-motion";
 import { animateFadeUp } from "../../constants/anim";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const NavBar = () => {
   const [selectedNavItem, setSelectedNavItem] = useState(navBarItems[0]);
-
+  const router = useRouter();
   return (
     <div className="absolute bottom-0 p-2 px-4 flex items-center justify-between w-full border-t-2 border-t-offWhite h-[60px]">
       {navBarItems.map((item, index) => {
@@ -21,7 +22,10 @@ const NavBar = () => {
               src={item.imgPath}
               alt={item.label}
               key={index}
-              onClick={() => setSelectedNavItem(item)}
+              onClick={() => {
+                setSelectedNavItem(item)
+                router.push(item.route)}
+              }
               className={cn("w-full h-8 mt-1 rounded-b-lg py-1", {"bg-offWhite" : item === selectedNavItem})}
             />
           </div>
